76a462da6faa74e03c7f98d19994736bbd3cd3ff
Nguyen Duc Phu <phunguyenduc99@gmail.com>
2023-11-13 19:51:45 +0700

    Store

---
+++ lab03/src/StoreTest.java
@@ -0,0 +1,22 @@
+
+public class StoreTest {
+
+    public static void main(String[] args) {
+
+        // Phu 20215116: Create a new Store
+        Store store = new Store();
+
+        // Phu 20215116: Create a new DVD object and add it to the Store
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        store.addDVD(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "Geogre Lucas", 87, 24.95f);
+        store.addDVD(dvd2);
+
+        // Phu 20215116: Test remove method
+        store.removeDVD(dvd1); // Phu 20215116: can remove
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        store.removeDVD(dvd3); // Phu 20215116: can't remove
+    }
+}
